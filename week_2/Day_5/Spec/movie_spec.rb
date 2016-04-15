47c431df7718923f00b3659984eb74dee8129a5a
Sarah Abogabir <abogabirsl@vcu.edu>
2016-04-15 14:55:10 -0400

    Add day5

---
+++ week_2/Day_5/Spec/movie_spec.rb
@@ -0,0 +1,3 @@
+require_relative ("../lib/movie.rb") #lib folder contains all classes
+
+RSpec.describe "My Movie Posters"
---
+++ week_2/Day_5/app_server.rb
@@ -0,0 +1,25 @@
+require"sinatra"
+require"imdb"
+require"sinatra/reloader" if development?
+
+require_relative"lib/movie.rb"
+
+get "/"
+	erb:(home)
+end 
+
+search = Movies.new
+
+#look from users persective
+#the goal is to have the most minimal information here
+get "/home" do #what user will type in search bar
+	erb :home
+end 
+
+post "/search" do #send info to server, user enters info
+	the_title =params[:search_title] 
+	@arr_movies = search.search_title(the_title) #or params..
+	erb :poster_page #needs to be tied to the name of the file 
+end
+
+# redirect to ("/")
