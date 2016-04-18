9c96510132d3a7d9f13526e91ef0fd13ddd2fc4d
Sarah Abogabir <abogabirsl@vcu.edu>
2016-04-18 14:15:58 -0400

    Day5, week 3

--- week_4/timetracking/app/controllers/site_controller.rb
+++ week_4/timetracking/app/controllers/site_controller.rb
@@ -6,4 +6,16 @@ class SiteController < ApplicationController #inheritance
 	def contact
 		render 'contact'
 	end
+
+	def say_name
+		@name = params[:name] #placeholder in the url parameter
+		render 'say_name'
+	end 
+
+	def calculate
+		@num1 = params [:first_num].to_f
+		@num2 = params [:second_num].to_f
+		@result = @num1 + @num2
+		render 'calculate'
+	end 
 end
