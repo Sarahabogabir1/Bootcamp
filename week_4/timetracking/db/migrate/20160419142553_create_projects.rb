ef1157da57df3520cf773a75bafceb5d3de688a1
Sarah Abogabir <abogabirsl@vcu.edu>
2016-04-19 15:14:06 -0400

    adding the changes

--- week_4/timetracking/db/migrate/20160419142553_create_projects.rb
+++ week_4/timetracking/db/migrate/20160419142553_create_projects.rb
@@ -1,6 +1,6 @@
 class CreateProjects < ActiveRecord::Migration
-  def change
-    create_table :projects do |t|
+  def change 
+    create_table :projects do |t| #describes the structure of the data base
       t.string :name
 
       t.timestamps null: false
--- week_4/timetracking/db/seeds.rb
+++ week_4/timetracking/db/seeds.rb
@@ -1,3 +1,14 @@
+Project.create(name:"Ironhack", description:"Ironhack is a Bootcamp")
+Project.create(name:"Final Project", description: "Final Ironhack project")
+Project.create(name: "Recipes", description: "Track  my favorite recipies")
+Project.create(name: "Names", description: "Names of students")
+Project.create(name: "Age", description: "Age of students" )
+Project.create(name: "Dogs", description: "Types of Dogs")
+Project.create(name: "Colors" , description: "Types of Colors")
+Project.create(name: "Cars", description: "Types of Cars")
+Project.create(name: "Candy", description: "Types of Candy")
+Project.create(name: "Singers" , description: "Types of Singers")
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
