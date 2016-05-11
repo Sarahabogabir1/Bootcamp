ae55a78144e187832e5c2a4401a96814303406b1
Sarah Abogabir <abogabirsl@vcu.edu>
2016-05-11 16:36:25 -0400

    made some changes to form

--- drones_united/app/controllers/review_controller.rb
+++ drones_united/app/controllers/review_controller.rb
@@ -4,10 +4,19 @@ class ReviewController < ApplicationController
 	end
 
 	def new
-  	@reviews = Reviews.new
+  		@reviews = Review.new
 	end
 
 	def create
-		@reviews = Reviews.new(params[:params])
+		@reviews = Review.new(params[:review])
+		if @reviews.save
+			redirect_to @user, ,alert:"User created successfully"
+		else 
+			redirect_to new_user_path, alert: "Error creating user."
+		end 
+	
+	def show
+    	@users = User.find(params[:id])
+	end
 end
 
